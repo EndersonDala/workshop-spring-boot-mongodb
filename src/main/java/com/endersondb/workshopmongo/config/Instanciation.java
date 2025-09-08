e363d8e22da2f79a5a6e8429aff5c832f1b15b14
Enderson Dala <endersondb@gmail.com>
2025-09-08 17:49:42 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/endersondb/workshopmongo/config/Instanciation.java
@@ -0,0 +1,29 @@
+package com.endersondb.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.endersondb.workshopmongo.domain.User;
+import com.endersondb.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instanciation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository rep;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		rep.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		rep.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}

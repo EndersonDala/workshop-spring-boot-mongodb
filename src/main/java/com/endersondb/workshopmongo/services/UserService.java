328b07c2629e7c0b4747d66788880c7af33a5a44
Enderson Dala <endersondb@gmail.com>
2025-09-09 15:27:11 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/endersondb/workshopmongo/services/UserService.java
+++ src/main/java/com/endersondb/workshopmongo/services/UserService.java
@@ -34,6 +34,11 @@ public class UserService {
 		return rep.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		rep.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}

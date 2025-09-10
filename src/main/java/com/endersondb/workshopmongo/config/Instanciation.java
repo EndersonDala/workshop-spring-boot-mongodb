ea3eb76a941fc37354dc01fe361b7928fcc22d37
Enderson Dala <endersondb@gmail.com>
2025-09-10 15:12:43 -0300

    Criando entity Post com user aninhado

--- src/main/java/com/endersondb/workshopmongo/config/Instanciation.java
+++ src/main/java/com/endersondb/workshopmongo/config/Instanciation.java
@@ -1,12 +1,16 @@
 package com.endersondb.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.endersondb.workshopmongo.domain.Post;
 import com.endersondb.workshopmongo.domain.User;
+import com.endersondb.workshopmongo.repository.PostRepository;
 import com.endersondb.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,15 +19,27 @@ public class Instanciation implements CommandLineRunner {
 	@Autowired
 	private UserRepository rep;
 	
+	@Autowired
+	private PostRepository prep;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		rep.deleteAll();
+		prep.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		rep.saveAll(Arrays.asList(maria, alex, bob));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		Post post1 = new Post(null, sdf.parse("09/09/2025"), "Partiu Viagem", "Vou viajar para São Paulo, abraços !", maria);
+		Post post2 = new Post(null, sdf.parse("10/09/2025"), "Bom dia", "Acordei feliz hoje", maria);
+		
+		prep.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/endersondb/workshopmongo/dto/PostDTO.java
@@ -0,0 +1,9 @@
+package com.endersondb.workshopmongo.dto;
+
+import java.io.Serializable;
+
+public class PostDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}

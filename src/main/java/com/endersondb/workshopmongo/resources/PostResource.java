fe6edd23af6b6f9d39c6c6c327bdc3107c7a9a11
Enderson Dala <endersondb@gmail.com>
2025-09-11 15:04:17 -0300

    Obtendo Post por Id

---
+++ src/main/java/com/endersondb/workshopmongo/resources/PostResource.java
@@ -0,0 +1,50 @@
+package com.endersondb.workshopmongo.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.endersondb.workshopmongo.domain.Post;
+import com.endersondb.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/post")
+public class PostResource {
+
+	@Autowired
+	private PostService serv;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post post = serv.findById(id);
+		return ResponseEntity.ok().body(post);	
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Post obj){
+		Post post = serv.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteById(@PathVariable String id){
+		serv.delete(id);
+		return ResponseEntity.noContent().build();	
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Post obj, @PathVariable String id){
+		Post post = new Post(obj.getId(), obj.getDate(), obj.getTitle(), obj.getBody(), obj.getAuthor());
+		post = serv.update(post);
+		return ResponseEntity.noContent().build();
+	}
+
+}
---
+++ src/main/java/com/endersondb/workshopmongo/services/PostService.java
@@ -0,0 +1,49 @@
+package com.endersondb.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.endersondb.workshopmongo.domain.Post;
+import com.endersondb.workshopmongo.repository.PostRepository;
+import com.endersondb.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository rep;
+	
+	public Post findById(String id) {
+		Optional<Post> post = rep.findById(id);
+		if (post.isPresent()) {
+			return post.get();
+		} else {
+			throw new ObjectNotFoundException("Post não encontrado.");
+		}
+	}
+	
+	public Post insert(Post obj) {
+		return rep.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		rep.deleteById(id);
+	}
+	
+	public Post update(Post obj) {
+		Post newObj = findById(obj.getId());
+		UpdateData(newObj, obj);
+		return rep.save(newObj);
+	}
+	
+	private void UpdateData(Post newObj, Post obj) {
+		newObj.setTitle(obj.getTitle());
+		newObj.setDate(obj.getDate());
+		newObj.setBody(obj.getBody());
+		newObj.setAuthor(obj.getAuthor());
+	}
+
+}
